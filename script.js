69640f776ca3fc3f8ef99601a7302a0cbe805205
melisaruiz <144286838+MelisaRuiz@users.noreply.github.com>
2023-09-15 01:03:38 -0400

    second commit

--- script.js
+++ script.js
@@ -1,69 +1,68 @@
-//const LIBRERO = ["APPLE", "TABLE", "MOUSE"];
-//let random = Math.floor(Math.random()*LIBRERO.length);
-//const SORTEO = LIBRERO[random];
-
-let sorteo = "APPLE";
-
-console.log(sorteo);
+const LIBRERO = ["APPLE", "TABLE", "MOUSE"];
+let random = Math.floor(Math.random()*LIBRERO.length);
+const sorteo = LIBRERO[random];
+//const sorteo = "APLLE";
+//let sorteo = "APPLE";
+//console.log(sorteo);
 let cant = 6;
 const BUTTON = document.getElementById("guess-button");
-BUTTON.disbled = true;
+//BUTTON.disbled = true;
 
-const API = "https://random-word-api.herokuapp.com/word?length=5"
+//const API = "https://random-word-api.herokuapp.com/word?length=5"
 
-fetch(API)
-    .then((response)=> response.json())
-    .then((response)=> {
-        sorteo = response[0].toUpperCase();
-        BUTTON.disbled = false;
-    })
-    .catch((err)=>{
-        console.log(err);
-        let random = Math.floor(random() * LIBRERO.length);
-        sorteo = LIBRERO[random];
-        BUTTON.disbled = false;
-    });
+//fetch(API)
+    //.then((response)=> response.json())
+    //.then((response)=> {
+        //sorteo = response[0].toUpperCase();
+        //BUTTON.disbled = false;
+    //})
+    //.catch((err)=>{
+        //console.log(err);
+        //let random = Math.floor(random() * LIBRERO.length);
+        //sorteo = LIBRERO[random];
+        //BUTTON.disbled = false;
+    //});
 
 BUTTON.addEventListener("click", ()=>{
     const INTENTO = leerIntento();
-    const GIRD = document.getElementById("gird");
+    const GRID = document.getElementById("grid");
     let row = document.createElement("div");
     row.className = "row";
     if (sorteo == INTENTO){
-        termianr("<h1> GANASTE! </h1>")
-    }
+        terminar("<h1>GANASTE! ;)</h1>");
+        }
     else {
         for (let i in sorteo){
             if (sorteo[i] === INTENTO[i]){
                 let cuadroLetra = armarLetra(INTENTO[i], "green");
                 row.appendChild(cuadroLetra);
-            }
+                }
             else if (sorteo.includes(INTENTO[i])){
                 let cuadroLetra = armarLetra(INTENTO[i], "#f3c237");
                 row.appendChild(cuadroLetra);
-            }
+                }
             else {
                 let cuadroLetra = armarLetra(INTENTO[i], "gray");
                 row.appendChild(cuadroLetra);
                 } 
         }
         cant--;
-    }
-    GIRD.appendChild(row)
-        if (cant == 0){
-            terminar("<h1> PERDISTE! </h1>");
         }
-    });
+    GRID.appendChild(row)
+    if (cant == 0){
+        terminar("<h1>PERDISTE! ;(</h1>");
+    }
 function leerIntento() {
     return document.getElementById("guess-input").value.toUpperCase();
     }
-function termianr (mensaje) {
+function terminar (mensaje) {
     document.getElementById("mensaje").innerHTML = mensaje;
     }
 function armarLetra(letra, color){
     let span = document.createElement("span");
     span.className = 'letter';
     span.innerHTML = letra;
-    span.style.borderBlockColor = color;
+    span.style.backgroundColor = color;
     return span
-    }
+    } 
+});
